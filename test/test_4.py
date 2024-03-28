808cfdcad4e31acf1afcc0a8b73b9161adbed850
Marcus Göstasson <m.gostasson@hotmail.com>
2024-03-28 09:44:20 +0100

    Added testmap

---
+++ test/test_4.py
@@ -0,0 +1,35 @@
+"""Testclass 4."""
+
+class :
+    """Test class to represent."""
+
+
+    def __init__(self):
+    """Initialize."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
+
+    def (self)
+    """."""
