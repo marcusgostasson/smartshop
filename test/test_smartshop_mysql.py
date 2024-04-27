f059b7b1a074706abc6b8363f89547e814df6961
Razmus R <Razmus.Rosen@hotmail.com>
2024-04-28 01:51:12 +0200

    Added tests for get_price_and_ingredients

--- test/test_smartshop_mysql.py
+++ test/test_smartshop_mysql.py
@@ -1,6 +1,5 @@
 import unittest
 import sys
-from unittest.mock import patch, MagicMock
 
 sys.path.append("C:/Users/Razmu/agile_development/SmartShop/smartshop/code/viewer")
 
@@ -22,7 +21,7 @@ class TestSmartshopMysql(unittest.TestCase):
 
     def test_get_price_and_ingredients_for_hamburger(self):
         db_instance = smartshop_mysql.SMARTSHOP_DB()
-        hamburger_res = db_instance.get_price_and_ingredients("hamburger")
+        hamburger_res = db_instance.get_price_and_ingredients("hamburgare")
         hamburger_ingredients = {'Willys': [('Blandfärs 20%', 49.9, 2), ('Salt Finkornigt', 7.9, 2),
                                             ('Svart Peppar Mellan Malen', 19.9, 2), ('Hamburgerbröd Frisco 4-p', 23.9, 2),
                                             ('Hamburgerost Cheddarsmak', 22.9, 2), ('Extra Virgin Fruttato Olivolja', 48.9, 2)],
@@ -33,6 +32,46 @@ class TestSmartshopMysql(unittest.TestCase):
                                           ('Hamburgerbröd Frisco 4-p', 23.9, 2), ('Hamburgerost Cheddarsmak', 32.5, 2),
                                           ('Olivolja Extra Virgin', 85.95, 2)]}
         self.assertEqual(hamburger_res, hamburger_ingredients)
+
+    def test_get_price_and_ingredients_for_kyckling_roma(self):
+        db_instance = smartshop_mysql.SMARTSHOP_DB()
+        kyckling_roma_res = db_instance.get_price_and_ingredients("kyckling roma")
+        kyckling_roma_ingredients = {'Willys': [('Körsbärstomater Klass 1', 14.9, 2),
+                                                ('Pasta Spaghetti', 22.9, 2),
+                                                ('Svart Peppar Mellan Malen', 19.9, 2),
+                                                ('Kycklingfile Fryst', 99.9, 2),
+                                                ('Salvia', 19.9, 2), ('Extra Virgin Fruttato Olivolja', 48.9, 2),
+                                                ('Charlotten Lök', 17.9, 2), ('Matlagningsvin Vitt 2.2%', 29.5, 2),
+                                                ('Strösocker', 21.9, 2), ('Zucchini', 14.9, 2), ('Parmigiano Reggiano Riven', 24.9, 2)],
+                                     'Ica': [('Körsbärstomater Klass 1', 22.95, 2), ('Pasta Spaghetti', 22.95, 2),
+                                             ('Svart Peppar Mellan Malen', 24.5, 2), ('Kycklingfile Fryst', 99.95, 2),
+                                             ('Salvia', 24.95, 2), ('Extra Virgin Olivolja', 55.95, 2), ('Charlotten Lök', 22.95, 2),
+                                             ('Matlagningsvin Vitt 2.2%', 39.5, 2), ('Strösocker', 25.5, 2), ('Zucchini', 18.48, 2),
+                                             ('Parmigiano Reggiano Riven', 38.95, 2)],
+                                     'Coop': [('Körsbärstomater Hela', 16.5, 2), ('Pasta Spaghetti', 23.5, 2),
+                                              ('Svart Peppar Malen', 27.95, 2), ('Kycklingfile Fryst', 116.0, 2), ('Salvia Eko', 24.95, 2),
+                                              ('Olivolja Extra Virgin', 85.95, 2), ('Charlotten Lök', 14.95, 2), ('Matlagningsvin Vitt 2%', 35.95, 2),
+                                              ('Strösocker', 27.95, 2), ('Zucchini', 14.95, 2), ('Parmigiano Reggiano Riven', 24.5, 2)]}
+        self.assertEqual(kyckling_roma_res, kyckling_roma_ingredients)
+
+    def test_get_price_and_ingredients_for_pasta_med_räkor_och_curry(self):
+        db_instance = smartshop_mysql.SMARTSHOP_DB()
+        pasta_med_räkor_och_curry_res = db_instance.get_price_and_ingredients("pasta med räkor och curry")
+        pasta_med_räkor_och_curry_ingredients = {'Willys': [('Pasta Spaghetti', 22.9, 2), ('Stora Skalade Räkor', 59.9, 2),
+                                                            ('Gul Lök', 14.9, 2), ('Vit Lök', 3.9, 2), ('Spetspaprika Röd', 21.9, 2),
+                                                            ('Champinjoner', 17.9, 2), ('Smör Normalsaltat 82%', 59.9, 2), ('Curry', 19.9, 2),
+                                                            ('Kokos Mjölk', 13.9, 2), ('Salt Finkornigt', 7.9, 2), ('Chilisås', 21.9, 2),
+                                                            ('Sambal Olek', 25.9, 2)],
+                                                 'Ica': [('Pasta Spaghetti', 22.95, 2),('Stora Skalade Räkor', 58.95, 2),
+                                                         ('Gul Lök', 19.95, 2), ('Vit Lök', 12.95, 2), ('Spetspaprika Röd', 21.95, 2),
+                                                         ('Champinjoner', 18.95, 2), ('Smör Normalsaltat 82%', 59.95, 2),
+                                                         ('Curry', 24.5, 2), ('Kokos Mjölk', 12.95, 2), ('Salt Finkornigt', 10.5, 2),
+                                                         ('Chilisås', 21.95, 2), ('Sambal Olek', 18.5, 2)],
+                                                 'Coop': [('Pasta Spaghetti', 23.5, 2), ('Räkor Extra Stora Skalade', 81.95, 2),
+                                                          ('Gul Lök', 14.95, 2), ('Vit Lök', 5.45, 2), ('Paprika Snack Röd', 29.95, 2),
+                                                          ('Champinjoner', 21.95, 2), ('Smör Normalsaltat 82%', 59.95, 2), ('Curry', 23.5, 2),
+                                                          ('Kokos Mjölk', 16.5, 2), ('Salt Finkornigt', 8.95, 2), ('Chilisås', 24.95, 2), ('Sambal Olek', 26.5, 2)]}
+        self.assertEqual(pasta_med_räkor_och_curry_res, pasta_med_räkor_och_curry_ingredients)
 
 
 if __name__ == '__main__':
