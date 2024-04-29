79d1d3c6682df3a874ae54d1afe6a8217c654e33
Razmus R <Razmus.Rosen@hotmail.com>
2024-04-29 15:05:53 +0200

    github action test

--- test/test_smartshop_mysql.py
+++ test/test_smartshop_mysql.py
@@ -22,7 +22,7 @@ class TestSmartshopMysql(unittest.TestCase):
 
     def test_get_price_and_ingredients_for_hamburger(self):
         db_instance = smartshop_mysql.SMARTSHOP_DB()
-        hamburger_res = db_instance.get_price_and_ingredients("tacos")
+        hamburger_res = db_instance.get_price_and_ingredients("hamburgare")
         hamburger_ingredients = {'Willys': [('Blandfärs 20%', 49.9, 2), ('Salt Finkornigt', 7.9, 2),
                                             ('Svart Peppar Mellan Malen', 19.9, 2), ('Hamburgerbröd Frisco 4-p', 23.9, 2),
                                             ('Hamburgerost Cheddarsmak', 22.9, 2), ('Extra Virgin Fruttato Olivolja', 48.9, 2)],
