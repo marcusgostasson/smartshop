1da5eb8f9fa2304f192cadfa0bad464a9a70268d
Razmus R <Razmus.Rosen@hotmail.com>
2024-05-15 14:38:35 +0200

    removed import

--- test/test_login_window.py
+++ test/test_login_window.py
@@ -5,22 +5,22 @@ from unittest.mock import MagicMock
 from PyQt5.QtWidgets import QApplication
 
 sys.modules["smartshop_mysql"] = MagicMock()
-sys.modules["login_window"] = MagicMock()
-
+sys.modules["create_user_window"] = MagicMock()
+sys.modules["start_window"] = MagicMock()
 
 script_dir = Path(__file__).resolve().parent
 sys.path.append(str(script_dir.parent.parent))
 
-from smartshop.code.py import create_user_window
+from smartshop.code.py import login_window
 
 
-class TestCreateUserWindow(unittest.TestCase):
-    def test_window_title(self):
+class TestLoginWindow(unittest.TestCase):
+    def test_window_title_login(self):
         """Testing if the name of the window is correct."""
         self.app = QApplication(sys.argv)
-        self.window = create_user_window.CreateUserWindow()
-        self.window.set_up_window()
-        self.assertEqual(self.window.windowTitle(), "Skapa användare")
+        self.window = login_window.LoginWindow()
+        self.window.set_up_login()
+        self.assertEqual(self.window.windowTitle(), "Logga in")
 
 
 if __name__ == "__main__":
